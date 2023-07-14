4cbb2183acb7fd324845718839ed010f63b1bb13
ivanlima096 <ivanlima096@gmail.com>
2023-07-14 12:06:10 -0300

    auth and register auth built successfully

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,56 +1,115 @@
-import { FaGamepad } from 'react-icons/fa'
-import './Login.css'
-import { Link } from 'react-router-dom'
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
-import { useState } from 'react'
-import { auth } from '../services/firebaseConfig'
+import { FaGamepad } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useState, useEffect } from 'react';
+import { auth } from '../services/firebaseConfig';
 
+import './Login.css';
 
 export default function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const [
     signInWithEmailAndPassword,
-    user,
-    loading,
-    error
-  ] = useSignInWithEmailAndPassword(auth)
+  ] = useSignInWithEmailAndPassword(auth);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        setLoading(false);
+      } else {
+        setUser(null);
+        setLoading(false);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   function handleSignIn(e) {
-    e.preventDefault()
+    e.preventDefault();
+    setLoading(true);
     signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }
+
+  function handleLogout() {
+    auth.signOut()
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   if (loading) {
-    return <div className='loader'></div>
+    return <div className='loader'></div>;
   }
+
   if (user) {
-    return console.log(user);
+    return (
+      <div className="login">
+        <div className="loginContainerLogged">
+          <p>Você já está logado.</p>
+          <Link to="/">Ir para a página inicial</Link>
+          <br />
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="login">
       <div className="loginContainer">
         <header className="header">
-          <FaGamepad className='icon' />
-          <p> Faça login para sua conta gamer!</p>
+          <FaGamepad className="icon" />
+          <p>Faça login para sua conta gamer!</p>
         </header>
         <form>
           <div className="inputContainer">
             <label htmlFor="email">Email</label>
-            <input type="text" name='email' id='email' placeholder='seuemail@email.com' onChange={e => setEmail(e.target.value)} />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="seuemail@email.com"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="inputContainer">
             <label htmlFor="password">Senha</label>
-            <input type="password" name='password' id='password' placeholder='***********' onChange={e => setPassword(e.target.value)} />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="***********"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
 
           <a href="">Esqueceu sua senha?</a>
-          <button className='button' onClick={handleSignIn}>
+          <button className="button" onClick={handleSignIn}>
             Entrar!
-
           </button>
+
 
           <div className="footer">
             <p>Ainda não tem uma conta?</p>
@@ -59,5 +118,5 @@ export default function Login() {
         </form>
       </div>
     </div>
-  )
+  );
 }
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,59 +1,96 @@
-import { FaGamepad } from 'react-icons/fa'
-import './Login.css'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
-import { auth } from '../services/firebaseConfig'
+import { FaGamepad } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '../services/firebaseConfig';
 
+import './Login.css';
 
 export default function Register() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const [
     createUserWithEmailAndPassword,
-    user,
-    loading,
-    error
-  ] = useCreateUserWithEmailAndPassword(auth)
+  ] = useCreateUserWithEmailAndPassword(auth);
 
   function handleRegister(e) {
-    e.preventDefault()
+    e.preventDefault();
+    setLoading(true);
     createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError("Erro ao criar conta, tente novamente!");
+        setLoading(false);
+      });
   }
 
+
   if (loading) {
-    return <p>carregando...</p>
+    return <div className='loader'></div>;
+  }
+
+  if (user) {
+    return (
+      <div className="login">
+        <div className="loginContainerLogged">
+          <p>Sua conta foi criada com sucesso!</p>
+          <Link to="/auth">Faça login agora!</Link>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="login">
       <div className="loginContainer">
         <header className="header">
-          <FaGamepad className='icon' />
-          <p> Preencha os dados para criar sua conta gamer!</p>
+          <FaGamepad className="icon" />
+          <p>Preencha os dados para criar sua conta gamer!</p>
         </header>
         <form>
           <div className="inputContainer">
             <label htmlFor="email">Email</label>
-            <input type="text" name='email' id='email' placeholder='seuemail@email.com' onChange={e => setEmail(e.target.value)} />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="seuemail@email.com"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="inputContainer">
             <label htmlFor="password">Senha</label>
-            <input type="password" name='password' id='password' placeholder='***********' onChange={e => setPassword(e.target.value)} />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="***********"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
 
-          <button className='button' onClick={handleRegister}>
+          <button className="button" onClick={handleRegister}>
             Criar!
-
           </button>
 
-          <div className="footer">
-            <p>Já possui uma conta?</p>
-            <Link to={"/auth"}>Fazer login agora!</Link>
-          </div>
+          {error && <p className="error">{error}</p>}
+          {!user && !error && (
+            <div className="footer">
+              <p>Já possui uma conta?</p>
+              <Link to="/auth">Fazer login agora!</Link>
+            </div>
+          )}
         </form>
       </div>
     </div>
-  )
+  );
 }
